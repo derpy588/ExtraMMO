7ddca2f606cf438d13a9a00a5c56b633814346e3
BuildTools <unconfigured@null.spigotmc.org>
2021-10-11 13:03:16 -0400

    Initial commit

---
+++ src/main/java/me/derpy/extrammo/Main.java
@@ -0,0 +1,22 @@
+package me.derpy.extrammo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+    //Stuff to do
+    //TODO: Skills/Stats System
+    //TODO: Item Builder
+    //TODO: Crafting Recipe Builder
+    //TODO: Mob Level System
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
